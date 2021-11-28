e7a3bbc2dbf2a3f5159f4c07671ccd1587e68c9d
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-11-28 13:01:46 +0530

    Update FibonacciSeries.java

--- week1/day1/assignments/FibonacciSeries.java
+++ week1/day1/assignments/FibonacciSeries.java
@@ -1,36 +1,20 @@
-package week1.day1.assignments;
+package week1.day1;
 
-public class FibonacciSeries {
+public class FibonacciSerious {	
+public static void main(String[] args) {
+int range = 8;
+int firstNum = 0;
+int secNum = 1;
+int sum;
+System.out.println("Fibonacci Series till " + range + " terms:");
+for(int i=1;i<=range;++i) {
+	System.out.print(firstNum + ", ");
 
-	/*
-	 * Goal: To find Fibonacci Series for a given range
-	 * 
-	 * input(range): 8 output: 0, 1, 1, 2, 3, 5, 8, 13
-	 * 
-	 * Shortcuts: 1) Print : type: syso, followed by: ctrl + space + enter 2) To
-	 * create a 'for' loop: type 'for', followed by ctrl + space + down arrow +
-	 * enter
-	 * 
-	 * What are my learnings from this code? 1) 2) 3)
-	 * 
-	 */
-
-	public static void main(String[] args) {
-
-		// initialize 3 int variables (Tip: range = 8, firstNum = 0, secNum = 1, sum in the series)
-
-		// Print first number
-		
-		// Iterate from 1 to the range
-		
-		// add first and second number assign to sum
-
-		// Assign second number to the first number
-
-		// Assign sum to the second number
-		
-		// print sum
-
-	}
-
+	sum =firstNum+secNum;
+	 firstNum=secNum;
+    	 secNum=sum;  
+	System.out.println("The count is:"+sum);
+			
+}
+}
 }
