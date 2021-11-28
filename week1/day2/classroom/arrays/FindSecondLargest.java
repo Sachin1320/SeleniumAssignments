0b6e1e6bdadd730d09f1d8e03e8ade46b4a38da7
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-11-28 18:32:59 +0530

    Update FindSecondLargest.java

--- week1/day2/classroom/arrays/FindSecondLargest.java
+++ week1/day2/classroom/arrays/FindSecondLargest.java
@@ -1,24 +1,17 @@
 package week1.day2.classroom.arrays;
 
+
 import java.util.Arrays;
-
 public class FindSecondLargest {
-
-	public static void main(String[] args) {
-
-		// Here is the input
+public static void main(String[] args) {
 		int[] data = {3,2,11,4,6,7};
-
-		
-		/*
-		 Pseudo Code:
-		 1) Arrange the array in ascending order
-		 2) Pick the 2nd element from the last and print it
-		 */
-		
+	    Arrays.sort(data);
+		int indexOfSecondLargest=data.length-2;
+		System.out.println(data[indexOfSecondLargest]);
 
 		
 	}
 
 	
 }
+		
