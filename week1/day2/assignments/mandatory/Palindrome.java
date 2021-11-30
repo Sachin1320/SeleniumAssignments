793d5d04ab712f32f1d2ad1f96afe4a8f77d91db
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-11-30 18:20:59 +0530

    Update Palindrome.java

--- week1/day2/assignments/mandatory/Palindrome.java
+++ week1/day2/assignments/mandatory/Palindrome.java
@@ -2,6 +2,20 @@ package week1.day2.assignments.mandatory;
 
 public class Palindrome {
 	//Build a logic to find the given string is palindrome or not
+	public static void main(String[] args) {
+		String str="madam";
+		String rev = "";
+		 int length = str.length(); 
+		 for ( int i = length - 1; i >= 0; i-- )  
+	         rev = rev + str.charAt(i);  
+			System.out.println(rev);
+		   if (str.equalsIgnoreCase(rev))  
+		         System.out.println("Given string is a palindrome"); 
+		   else
+			   System.out.println("Given string is not a Palindrome");
+	}
+		
+	}
      
 	/*
 	 * Pseudo Code
