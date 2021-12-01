6595e2738467cc411661a6f9801be4f43d7c9da6
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-12-01 16:07:20 +0530

    Update CharOccurance.java

--- week1/day2/classroom/string/CharOccurance.java
+++ week1/day2/classroom/string/CharOccurance.java
@@ -3,9 +3,28 @@ package week1.day2.classroom.string;
 public class CharOccurance {
 	
 	public static void main(String[] args) {
+			String str = ("welcome to chennai");
+			int count=0;
+			
+			System.out.println(str);
+			
+			char charArray[]=str.toCharArray();
+			int len=charArray.length;
+			
+			for(int i=0;i<len-1;i++)
+			{
+				if(charArray[i]=='e')
+					count++;
+					
+					
+			}
+			
+			if(count>0)
+				System.out.println("The number of time 'e' occured is "+count );
+		}
+
+	}
 		// Check number of occurrences of a char (eg 'e') in a String
-		
-			String str = "welcome to chennai";
 
 			// declare and initialize a variable count to store the number of occurrences
 			
