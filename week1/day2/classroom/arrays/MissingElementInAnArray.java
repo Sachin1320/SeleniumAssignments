e0a6d7fb9342c5ba43ff9847a8fabb1827d50219
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-12-14 07:54:50 +0530

    Update MissingElementInAnArray.java

--- week1/day2/classroom/arrays/MissingElementInAnArray.java
+++ week1/day2/classroom/arrays/MissingElementInAnArray.java
@@ -1,10 +1,34 @@
 package week1.day2.classroom.arrays;
+import java.util.ArrayList;
+
+import java.util.List;
 
 public class MissingElementInAnArray {
 
 	public static void main(String[] args) {
 		// Here is the input
 		int[] arr = {1,2,3,4,7,6,8};
+		
+		List<Integer> val=new ArrayList<Integer>();
+		for(int var:arr)
+		{
+			val.add(var);
+		}
+		
+		System.out.println(val);
+		
+		
+		for(int i=1;i<val.size();i++)
+		{
+			if(val.get(i-1)!=i)
+				{System.out.println(i);
+			break;
+				}
+		}
+
+	}
+
+}
 
 		// Sort the array	
 		
