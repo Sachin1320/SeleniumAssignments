16afee8f17d637bae97b2acc0760d1197bda927c
Sachin1320 <95136152+Sachin1320@users.noreply.github.com>
2021-12-14 07:32:09 +0530

    Update RemoveDuplicates.java

--- week1/day2/assignments/mandatory/RemoveDuplicates.java
+++ week1/day2/assignments/mandatory/RemoveDuplicates.java
@@ -1,7 +1,21 @@
 package week1.day2.assignments.mandatory;
-
+import java.util.LinkedHashSet;
+import java.util.Set;
 public class RemoveDuplicates {
-	
+	public static void main(String[] args) {
+		String i = "Paypal India";
+		char[] input = i.toCharArray();
+		 Set<Character> charset = new LinkedHashSet<Character>();
+		for(Character  dupcharset : input) 
+			{
+			if (dupcharset != ' ')
+				charset.add(dupcharset);
+			}
+            System.out.println(charset);
+	}}
+
+
+
 	/*
 	 * Pseudo code 
 	 
